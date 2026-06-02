5086f93f9379eaf2a742820e4e9bf55f77b0e9ce
Adan Adeel <adanadeel903@gmail.com>
2026-06-02 15:48:57 +0500

    auth controller set up

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { configDotenv } from "dotenv"
+import cookieParser from "cookie-parser"
+
 
 const app = express() 
-
+app.use(cookieParser())
 app.use(express.json())
 
 
--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import { User } from "../models/user.model";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { configDotenv } from "dotenv"
 
-
+const jwt_secret = process.env.JWT_SECRET
 async function registerUser(req,res) {
 
     const {name, email, password} = req.body
@@ -12,6 +15,31 @@ async function registerUser(req,res) {
     if(isUserAlreadyExit){ 
         return res.status(400).json({ 
             message: "User already exists"
-        })
-    }
-}
+    })
+    
+    const hashedPassword = await bcrypt.hash(password,10)
+    const user = await userModel.create({
+        name, 
+        email, 
+        password : hashedPassword
+    })
+    const token = jwt.sign({
+        id: user._id
+
+    },jwt_secret)
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message: "User registered Successfully",
+        user : { 
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        }
+    })
+
+}}
+
+
+export {registerUser}
