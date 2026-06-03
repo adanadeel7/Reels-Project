0faa1780b335a3f9e54d1ed9b170bd64356dc15e
Adan Adeel <adanadeel903@gmail.com>
2026-06-03 20:08:55 +0500

    Login Api Added

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -43,8 +43,42 @@ async function registerUser(req,res) {
 
 }
 
-async function loginUser() { 
-    console.log("Hello")
+async function loginUser(req,res) {
+    const {email, password} = req.body() 
+
+    const user = await User.findOne({
+        email
+    })
+
+
+    if (!user) { 
+        res.status(400).json({
+            message : "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) { 
+        res.status(400).json({
+            message : "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id
+    },jwt_secret)
+    
+    res.cookie("token",token)
+    
+    res.status(200).json({
+        message: "User Login Successfully",
+        user : { 
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        }
+    })
 }
 
 
