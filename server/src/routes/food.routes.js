002dbc413d3890198c9c718a67578eded7d7cd56
Adan Adeel <adanadeel903@gmail.com>
2026-08-19 11:40:28 +0500

    Food Routes Completed

--- server/src/routes/food.routes.js
+++ server/src/routes/food.routes.js
@@ -1,11 +1,23 @@
 import express from "express"; 
-import createFood from "../controllers/food.controller.js";
-import {authFoodPartnerMiddleware} from "../middlewares/auth.middleware.js";
-
+import createFood, { getFoodItems, getSaveFood, likeFood,saveFood } from "../controllers/food.controller.js";
+import {authFoodPartnerMiddleware,authUserMiddleware} from "../middlewares/auth.middleware.js";
+import multer from 'multer'
 const foodRouter = express.Router()
+
+const upload = multer({
+    storage : multer.memoryStorage()
+})
+
+
 
 foodRouter.post('/',authFoodPartnerMiddleware,createFood)
 
-foodRouter.get("/", au)
+foodRouter.get("/", authUserMiddleware, getFoodItems)
+
+foodRouter.post('/like', authUserMiddleware, likeFood)
+
+foodRouter.post('/save', authUserMiddleware,saveFood)
+
+foodRouter.get('/save', authUserMiddleware , getSaveFood)
 
 export default foodRouter
