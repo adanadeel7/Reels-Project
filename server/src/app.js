2ea6f8b0b634fb377df32016c368ff1b92db5a72
Adan Adeel <adanadeel903@gmail.com>
2026-08-20 02:17:34 +0500

    Backend Completed

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,27 @@ import { configDotenv } from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
 import foodRoutes from "./routes/food.routes.js"
-
+import cors from 'cors'
+import foodPartnerRoutes from "./routes/foodPartner.routes.js"
 
 
 const app = express() 
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json())
 
+app.get("/", (req,res) => { 
+    res.send("Hello World!")
+})
 
 app.use('/api/auth', authRoutes)
 app.use('/api/food', foodRoutes)
+app.use('/api/food-partner', foodPartnerRoutes)
+
+
 
 export default app;
